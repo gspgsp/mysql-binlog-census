cef512a5a9edf5577d54f6064ce46d84d00b1507
guoshipeng <2249875980@qq.com>
2021-03-03 10:25:24 +0800

    修改tests异常

--- tests/BinlogTest.php
+++ tests/BinlogTest.php
@@ -19,6 +19,12 @@ use PHPUnit\Framework\TestCase;
 
 final class BinlogTest extends TestCase
 {
+    /**
+     * @Notes: 测试当前实例有没有按规参数传参
+     * @Author: guoshipeng
+     * @Time: Times
+     * @Interface testGetBinlogWithInvalidParam
+     */
     public function testGetBinlogWithInvalidParam()
     {
         // 断言会出现这个异常，那么当出现这个异常的时候，不会报错；如果没有这个断言，那么出现异常就会报错；当正常运行的时候也不会有任何问题
@@ -30,5 +36,22 @@ final class BinlogTest extends TestCase
 
         // 如果没有抛出异常就会运行到这里，标识当前测试没有成功
         $this->fail('缺少必要参数。。。');
+    }
+
+    /**
+     * @Notes: 测试当前实例所属类
+     * @Author: guoshipeng
+     * @Time: Times
+     * @Interface testGetInstance
+     */
+    public function testGetInstance()
+    {
+        // 如果没有这个异常断言，那么执行到下面行，由于没有传参数，会抛出异常；如果有这个异常断言，就不会抛出异常
+        $this->expectException(InvalidArgumentException::class);
+
+        $binlog = new Binlog();
+
+        // 断言当前实例为指定类型
+        $this->assertInstanceOf(Binlog::class, $binlog);
     }
 }
